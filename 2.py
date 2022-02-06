e64c195656cdf10b5714b31261744afda1e9652b
Anushan100 <85163876+Anushan100@users.noreply.github.com>
2022-02-06 14:57:02 +0530

    Python Basics, Conditional Loops

---
+++ 2.py
@@ -0,0 +1,5 @@
+firstname=str(input("enter the firstname"))
+lastname=str(input("enter the lastname"))
+re=firstname[::-1]
+re1=lastname[::-1]
+print(re+" "+re1)
---
+++ 3.py
@@ -0,0 +1,5 @@
+diameter=12
+radius=12/2
+pi=3.14
+volume_of_the_sphere=float(4/3*3.14*radius**3)
+print(volume_of_the_sphere)
